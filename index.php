06a2a9f078fb10933322fcd21f11acadc02d3e37
theomehl <ted.mehl@gmail.com>
2018-02-02 09:29:40 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<styles>
+body {
+    font-face: sans-serif;
+}
+.flex {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</styles>
+<div class="flex">
+  <div><h1>Coming Soon &trade;</h1></div>
+</div>
